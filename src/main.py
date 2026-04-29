61cbe3d2716b94c78ffc9b82f5a7af62464d4056
Marcel12345456 <229952693+Marcel12345456@users.noreply.github.com>
2026-04-29 14:19:07 +0200

    added iterations and results to csv

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
 from src.board import random_endgame
 from src.tablebase_api import get_tablebase_data
-from src.engine import evaluate_board
+from src.engine import play_game
+import csv
 
-currentBoard = random_endgame()
-fen = currentBoard.fen()
+results = []
+depth = 6
 
+for i in range(1):
+    currentBoard = random_endgame()
+    fen = currentBoard.fen()
+    engine_result = get_tablebase_data(fen)
+    heuristic_result = play_game(currentBoard, depth)
 
-engine_result = get_tablebase_data(fen)
+    results.append({
+        "iteration": i + 1,
+        "fen": fen,
+        "engine": engine_result.json()["dtm"],
+        "heuristic": heuristic_result,
+        "depth": depth,
+    })
 
-heuristic_result = evaluate_board(currentBoard)
+with open('results.csv', 'w', newline='') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=['iteration', 'fen', 'heuristic', 'engine', 'depth'])
+    writer.writeheader()
+    writer.writerows(results)
 
-
-print("FEN: ", fen)
-print("Mate based on heuristics: ", heuristic_result)
-print("Mate based on engine: ", engine_result.json()["dtm"])
+for r in results:
+    print(f"Run {r['iteration']}: Engine={r['engine']}, Heuristic={r['heuristic']}")
