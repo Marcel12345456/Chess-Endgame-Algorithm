a55423c097ca514295cb796250534549b4eb139c
Marcel12345456 <marcel.ober99@gmx.de>
2026-04-29 15:24:30 +0200

    update engine.py: simplify and optimize

--- src/engine.py
+++ src/engine.py
@@ -17,14 +17,12 @@ def extract_positions(current_board):
     # Nummer des Feldes herausfinden (0-63)
     white_king_square = list(white_king_on_board)[0]
     black_king_square = list(black_king_on_board)[0]
-
     return white_king_square, black_king_square
 
 # Koordinaten des Feldes anhand der Königsposition herausfinden
 def get_coordinates(piece_position):
     x = piece_position % 8
     y = piece_position // 8
-
     return x, y
 
 def distance_to_center (black_king_square):
@@ -32,15 +30,12 @@ def distance_to_center (black_king_square):
     # Größter Wert (Abstand zur Mitte) ist 3, kleinster Wert ist 0
     distance_x = max(3-black_king_x, black_king_x-4)
     distance_y = max(3-black_king_y, black_king_y-4)
-
     return distance_x + distance_y
 
 def manhattan_distance(white_king_square, black_king_square):
-
     white_king_x, white_king_y = get_coordinates(white_king_square)
     black_king_x, black_king_y = get_coordinates(black_king_square)
     manhattan_distance_score = abs(white_king_x - black_king_x) + abs(white_king_y - black_king_y)
-
     return 14 - manhattan_distance_score
 
 def rook_positioning(current_board, black_king_square):
@@ -53,17 +48,13 @@ def rook_positioning(current_board, black_king_square):
     black_king_x, black_king_y = get_coordinates(black_king_square)
     vertical_distance = abs(black_king_y - rook_y)
     horizontal_distance = abs(black_king_x - rook_x)
-
     return abs(vertical_distance - horizontal_distance)
 
 def minimax(current_board, depth, ply=0, alpha=-INF, beta=INF):
     white_to_move = (current_board.turn == chess.WHITE)
-    # abbrechen, wenn:
     if current_board.is_checkmate():
         return black_is_mated - ply, None
-    if current_board.is_stalemate():
-        return stalemate, None
-    if current_board.is_insufficient_material() or current_board.can_claim_draw():
+    if current_board.is_insufficient_material() or current_board.can_claim_draw() or current_board.is_stalemate():
         return stalemate, None
     if depth == 0:
         return evaluate_board(current_board), None
@@ -103,13 +94,10 @@ def minimax(current_board, depth, ply=0, alpha=-INF, beta=INF):
         return best_score, best_move
 
 def evaluate_board(current_board):
-
     white_king_square, black_king_square = extract_positions(current_board)
-
     score = center_weight * distance_to_center(black_king_square)
     score += manhattan_weight * manhattan_distance(white_king_square, black_king_square)
     score += rook_weight * rook_positioning(current_board, black_king_square)
-
     return score
 
 def play_game(current_board, depth):
